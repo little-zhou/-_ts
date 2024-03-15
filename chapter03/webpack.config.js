267514147ab767d931fd5a44f26fe9f667c8b37d
little-zhou <424048725@qq.com>
2024-03-15 21:45:20 +0800

    webpack package code 20240315

---
+++ chapter03/webpack.config.js
@@ -0,0 +1,51 @@
+// 引入一个包
+const path = require('path');
+// 引入html插件
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+// webpack的所有配置信息都应该写在module.exports中
+module.exports = {
+
+  mode: 'development',
+
+  // 指定入口文件
+  entry: './src/index.ts',
+
+  // 指定打包文件所在目录
+  output: {
+    // 打包之后文件的目录
+    path: path.resolve(__dirname, 'dist'),
+    // 打包之后文件的名字
+    filename: "bundle.js",
+    clean: true
+  },
+
+  // 配置webopack打包时需要使用的文件
+  module: {
+    // 指定加载的规则
+    rules: [
+      {
+        // test指定规则生效的文件
+        test: /\.ts$/,
+        // 要使用的loader
+        use: 'ts-loader',
+        // 指定要排除的文件
+        exclude: /node_modules/
+      },
+    ]
+  },
+
+  // 配置webpack插件
+  plugins: [
+    new HTMLWebpackPlugin({
+      // title: "这是一个自定义title"
+      template: "./src/index.html"
+    }),
+  ],
+
+  // 用来设置引用模块(ts,js文件可以作为模块引用)
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+
+};
